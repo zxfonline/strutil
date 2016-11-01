1c1a666d1960f530152f19ff000ec3438d0bcec8
zxfonline <zxfonline@sina.com>
2016-11-01 20:15:16 +0800

    add some fun
    add some fun

--- strutil.go
+++ strutil.go
@@ -2,8 +2,10 @@ package strutil
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var (
@@ -165,4 +167,90 @@ func Stof64(v string, def ...float64) float64 {
 	} else {
 		panic(fmt.Errorf("invalid float64 param:%v", v))
 	}
+}
+
+func Radians(degrees float64) float64 {
+	return degrees * math.Pi / 180
+}
+
+func Degrees(radians float64) float64 {
+	return radians * 180 / math.Pi
+}
+
+func Median(items []float64) float64 {
+	n := len(items)
+	switch {
+	case n == 0:
+		return 0
+	case n%2 == 1:
+		return items[n/2]
+	default:
+		a := items[n/2-1]
+		b := items[n/2]
+		return (a + b) / 2
+	}
+}
+
+func DurationString(d time.Duration) string {
+	h := int(d.Hours())
+	m := int(d.Minutes()) % 60
+	s := int(d.Seconds()) % 60
+	return fmt.Sprintf("%d:%02d:%02d", h, m, s)
+}
+
+func ParseFloats(items []string) []float64 {
+	result := make([]float64, len(items))
+	for i, item := range items {
+		f, _ := strconv.ParseFloat(item, 64)
+		result[i] = f
+	}
+	return result
+}
+
+func ParseInts(items []string) []int {
+	result := make([]int, len(items))
+	for i, item := range items {
+		f, _ := strconv.ParseInt(item, 0, 0)
+		result[i] = int(f)
+	}
+	return result
+}
+
+func Fract(x float64) float64 {
+	_, x = math.Modf(x)
+	return x
+}
+
+func Clamp(x, lo, hi float64) float64 {
+	if x < lo {
+		return lo
+	}
+	if x > hi {
+		return hi
+	}
+	return x
+}
+
+func ClampInt(x, lo, hi int) int {
+	if x < lo {
+		return lo
+	}
+	if x > hi {
+		return hi
+	}
+	return x
+}
+
+func ShowProgress(start time.Time, rays uint64, crtValue, totalValue int) {
+	pct := int(100 * float64(crtValue) / float64(totalValue))
+	elapsed := time.Since(start)
+	fmt.Printf("\r%4d / %d (%3d%%) [", crtValue, totalValue, pct)
+	for p := 0; p < 100; p += 3 {
+		if pct > p {
+			fmt.Print("=")
+		} else {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Printf("] %s ", DurationString(elapsed))
 }
