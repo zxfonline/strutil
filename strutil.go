e8d60818d93ffa52355d721c1167e33b5077dfd3
zxfonline <zxfonline@sina.com>
2017-04-13 10:11:45 +0800

    format

--- strutil.go
+++ strutil.go
@@ -1,6 +1,7 @@
 // Copyright 2016 zxfonline@sina.com. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
 package strutil
 
 import (
