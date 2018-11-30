2f623b9d5db0ad3bf7400ddca8bae4ed2709be1e
zhangxianfu <359168950@qq.com>
2018-11-30 16:40:38 +0800

    Update strutil.go

--- strutil.go
+++ strutil.go
@@ -721,6 +721,8 @@ func CheckStrLen(str string, minLen, maxLen int) bool {
 	return length >= minLen && length <= maxLen
 }
 
+var rd = rand.NewSource(time.Now().UnixNano())
+
 const (
 	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	letterIdxBits = 6                    // 6 bits to represent a letter index
@@ -733,7 +735,7 @@ func RandStr(n int) string {
 	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
 	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
-			cache, remain = rand.Int63(), letterIdxMax
+			cache, remain = rd.Int63(), letterIdxMax
 		}
 		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
 			b[i] = letterBytes[idx]
